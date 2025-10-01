3fe020f3bcea1ee15ef5d259aefc3c4636c79fa9
prajwal06 <prajwaljadhav057@gmail.com>
2025-10-01 15:59:38 +0530

    s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import UserSignin from "./pages/UserSignin";
-import CaptainSignIn from "./pages/CaptainSignin";
+import CaptainSignIn from "./pages/CaptainSignIn";
 import UserSignup from "./pages/UserSignup";
 import CaptainSignup from "./pages/CaptainSignup";
 import Navbar from "./components/Navbar";
